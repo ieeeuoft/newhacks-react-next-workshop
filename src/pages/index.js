b1f490625332c8062a1bf128f0e51492f5ae7f93
Scott Hirano <scottprotoss@gmail.com>
2023-10-28 17:32:49 -0400

    first commit, not working

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,54 @@ export default function App() {
   const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState('');
 
-  const addMessage = (from, msg) => {
-    const newMessage = { from, msg };
+  const addMessage = (role, message) => {
+    const newMessage = { role, message };
     setMessages((prevMessages) => [...prevMessages, newMessage]);
   };
 
-  const simulateBotResponse = (userMessage) => {
-    setTimeout(() => {
-      const botResponse = `Bot response to: ${userMessage}`;
-      addMessage('bot', botResponse);
-    }, 100);
-  };
+  const simulateBotResponse = async (userMessage) => {
+    // setTimeout(() => {
+    //   const botResponse = `Bot response to: ${userMessage}`;
+    //   addMessage('bot', botResponse);
+    // }, 100);
+    console.log("user message: " + userMessage)
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: 'Bearer 1lKjIkzPneTDcPCMyYf0d2ehJvfGAKbqgj3x96Ed' // This is your trial API key
+      },
+      body: JSON.stringify({
+          message: userMessage,
+          model: 'command',
+          stream: true,
+          chat_history: messages,
+          prompt_truncation: 'auto',
+          connectors: [{"id":"web-search"}],
+          citation_quality: 'accurate',
+          documents: [],
+          temperature: 0.3
+        })
+      }
+      console.log(options.body)
+      const res = await fetch('https://api.cohere.ai/v1/chat', options)
+      const content = await res.json()
+      console.log(content)
+    }
 
-  const handleUserInput = () => {
+    
+
+
+  const handleUserInput = async () => {
     const userMessage = inputText.trim();
     if (userMessage) {
       addMessage('user', userMessage);
       setInputText('');
-      simulateBotResponse(userMessage);
+      await simulateBotResponse(userMessage);
     }
   };
-
+  
   useEffect(() => {
     const chatContainer = document.getElementById('chat-container');
     chatContainer.scrollTop = chatContainer.scrollHeight; // bruh scroll to bottom lets go
@@ -36,9 +63,9 @@ export default function App() {
       {messages.map((message, index) => (
         <div
           key={index}
-          className={`chat-bubble ${message.from === 'user' ? 'user' : 'bot'}`}
+          className={`chat-bubble ${message.role === 'user' ? 'user' : 'bot'}`}
         >
-          {message.msg}
+          {message.message}
         </div>
       ))}
       <div className="input-container">
