12ed08be8f744f2c7e953bb75de9e940722e1132
Scott Hirano <scottprotoss@gmail.com>
2023-11-04 13:43:37 -0400

    Sample template complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
-import NavBar from './NavBar';
 
 export default function App() {
   const [messages, setMessages] = useState([]);
+
+  // State variable to keep input text
   const [inputText, setInputText] = useState('');
-  const [disableInput , setDisableInput] = useState(false);
-  const [dark , setDark] = useState(false);
   
+  // TODO: make a state variable to keep track of dark or light mode
+
   const addMessage = (role, message) => {
     const newMessage = { role, message };
     setMessages((prevMessages) => [...prevMessages, newMessage]);
   };
-
+  
   const simulateBotResponse = async (userMessage) => {
-    setDisableInput(true);
+    // API Options
     const options = {
       method: 'POST',
       headers: {
@@ -23,32 +24,29 @@ export default function App() {
         authorization: process.env.NEXT_PUBLIC_API_KEY // This is your trial API key
       },
       body: JSON.stringify({
-          message: userMessage,
-          model: 'command-light',
-          chat_history: messages,
-          prompt_truncation: 'auto',
-          connectors: [{"id":"web-search"}],
-          citation_quality: 'accurate',
-          documents: [],
-          temperature: 0.3
-        })
-      }
-      console.log(options.body)
-      try{
-      const res = await fetch('https://api.cohere.ai/v1/chat', options)
-      let content = await res.json()
-      
-      console.log(content.text)
-      addMessage ('Chatbot', content.text);
-
-      } catch (err) {
-        console.error(err)
-      }
-
-      setDisableInput(false);
+        message: userMessage,
+        model: 'command-light',
+        chat_history: messages,
+        prompt_truncation: 'auto',
+        connectors: [{"id":"web-search"}],
+        citation_quality: 'accurate',
+        documents: [],
+        temperature: 0.3
+      })
     }
 
-    const handleUserInput = async () => {
+    try {
+      // API link: https://api.cohere.ai/v1/chat
+      // TODO: Make a request to the API with the options above
+
+      // TODO: Add your response to the chat using the addMessage function
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const handleUserInput = async () => {
     const userMessage = inputText.trim();
     if (userMessage) {
       addMessage('user', userMessage);
@@ -62,19 +60,34 @@ export default function App() {
     chatContainer.scrollTop = chatContainer.scrollHeight; // bruh scroll to bottom lets go
   }, [messages]);
 
+  // TODO: Make an alert every time the user switches between light and dark mode
+
+  
+  useEffect(() => {
+    // TODO : add a welcome message by sending "hi" to the bot when the page loads
+
+  }, [])
   
 
-  useEffect(() => {
-    const hi = async () => {
-      addMessage('user', 'hi')
-      await simulateBotResponse("hi")
-    }
-    hi()
-  }, [])
+
   return (
   <>
-    <NavBar dark={dark} />
-    <div className= {dark ? "chat-container dark" : "chat-container"} id="chat-container">
+    {/* Navbar */}
+    <div className="navbar">
+      <Link href={"https:/ieee.utoronto.ca"}>
+        <img className='logo' src="/logo.png"/>
+      </Link>
+      <Link href={"/"}>
+        <button className="promo">Home</button>
+      </Link>
+      {/* TODO: Make the promo page */}
+      <Link href={"/promo"}>
+        <button className="promo">Promo</button>
+      </Link>
+    </div>
+
+    {/* Chat */}
+    <div className="chat-container" id="chat-container">
       {messages.map((message, index) => (
         <div
           key={index}
@@ -84,22 +97,27 @@ export default function App() {
         </div>
       ))}
     </div>
-    <div className= { `input-container ${ dark && 'dark'}`}>
-        <button onClick={()=>setDark(!dark)}> {dark ? "enable light" : "enable dark"}</button>
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          placeholder= { disableInput ? "Loading bot response.." : "Type a message..."} 
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleUserInput();
-            }
-          }}
-          disabled={disableInput}
-        />
-        <button onClick={handleUserInput} disabled={disableInput} >Send</button>
-      </div>
+
+    {/* Input */}
+    <div className="input-container">
+      {/* Light and Dark Mode Button */}
+      <button 
+        // TODO: handle user switching between light and dark mode using onClick
+
+      >
+        Dark Mode
+      </button>
+      <input
+        type="text"
+        value={inputText}
+        // TODO: handle user input using onChange
+
+        placeholder="Type a message..." 
+        // TODO: handle a user pressing enter to send a message
+
+      />
+      <button onClick={handleUserInput}>Send</button>
+    </div>
   </>
   );
 }
